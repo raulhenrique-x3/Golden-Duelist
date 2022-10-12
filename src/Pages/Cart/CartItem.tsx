73fb0c4fad9fa7a87ea3712865ba0246a64d3877
Raul Henrique <raulheri43@hotmail.com>
2022-10-11 23:07:49 -0300

    atualizações no carrinho de compra e mudanças no layout do webapp

--- src/Pages/Cart/CartItem.tsx
+++ src/Pages/Cart/CartItem.tsx
@@ -3,10 +3,10 @@ import { RootState } from "../../Redux/store";
 import { useDispatch, useSelector } from "react-redux";
 import { BsTrashFill } from "react-icons/bs";
 import Marquee from "react-fast-marquee";
-import { ICard } from "../../Interfaces/interfaces";
 import { removeFromCart } from "../../Redux/Features/cartSlice";
 import { Button } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const CartItem = () => {
   const cart = useSelector((state: RootState) => state.cart);
@@ -16,7 +16,15 @@ export const CartItem = () => {
   }
   const navigate = useNavigate();
 
-  if (cart.cartItems.length == 0) {
+  const [totalValue, setTotalValue] = useState(0);
+  const totalShipping = 10;
+  useEffect(() => {
+    let initialValue = 0;
+    cart.cartItems.map((card) => (initialValue += card.cartQuantity * card.card_prices[0].cardmarket_price));
+    setTotalValue(initialValue);
+  }, [cart]);
+
+  if (cart.cartItems.length === 0) {
     return (
       <div className={styles.emptyCart}>
         <p className={styles.emptyCartMsg}>CARRINHO VAZIO... :/</p>
@@ -27,23 +35,48 @@ export const CartItem = () => {
     );
   } else
     return (
-      <>
-        {cart.cartItems.map((item) => (
-          <div className={styles.cartItemContainer} key={item.id}>
-            <img src={item?.card_images[0].image_url} alt={item?.name} className={styles.itemImage} />
-            <div className={styles.itemInfo}>
-              <span className={styles.itemNameIcon}>
-                <Marquee gradient={false} speed={5} className={styles.itemName}>
-                  {item?.name}
-                </Marquee>
-                <BsTrashFill className={styles.BsTrashFill} onClick={() => handleRemoveFromCart(item.id)} />
-              </span>
+      <div className={styles.cartFullItems}>
+        <div className={styles.cartCard}>
+          {cart.cartItems.map((item) => (
+            <div className={styles.cartItemContainer} key={item.id}>
+              <img src={item?.card_images[0].image_url} alt={item?.name} className={styles.itemImage} />
+              <div className={styles.itemInfo}>
+                <span className={styles.itemNameIcon}>
+                  <Marquee gradient={false} speed={5} className={styles.itemName}>
+                    {item?.name}
+                  </Marquee>
+                  <BsTrashFill className={styles.BsTrashFill} onClick={() => handleRemoveFromCart(item.id)} />
+                </span>
 
-              <p className={styles.itemValue}>Subtotal: ${item?.card_prices[0].cardmarket_price}</p>
-              <p className={styles.itemQnt}>Qnt: {item?.cartQuantity}</p>
+                <p className={styles.itemValue}>Subtotal: ${item?.card_prices[0].cardmarket_price}</p>
+                <p className={styles.itemQnt}>Qnt: {item?.cartQuantity}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.boxBuyInfo}>
+          <div className={styles.boxBuyCep}>
+            <p className={styles.boxBuyTitle}>Entrega</p>
+            <p className={styles.boxBuySubtitle}>Calcular frete para o CEP:</p>
+            <div className={styles.inputButtonCep}>
+              <input type={"text"} placeholder={"Meu cep"} className={styles.cepInput} />
+              <Button colorScheme={"blue"} size="md">
+                Calcular
+              </Button>
             </div>
           </div>
-        ))}
-      </>
+          <div className={styles.totalItemsInfo}>
+            <span className={styles.subtotal}>
+              <p className={styles.subtotalText}>Subtotal: </p>
+              <p className={styles.subtotalValue}>${totalValue.toFixed(2)}</p>
+              <p className={styles.totalShippingText}>Frete: </p>
+              <p className={styles.totalShippingValue}>$ {totalShipping}</p>
+              <p className={styles.total}>Total: </p>
+              <p className={styles.totalValue}>${totalValue + totalShipping}</p>
+            </span>
+          </div>
+        </div>
+      </div>
     );
 };
--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,9 +1,49 @@
+import { Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsApple } from "react-icons/bs";
+import { SiGmail } from "react-icons/si";
 import styles from "./loginPage.module.scss";
 
 export const LoginPage = () => {
   return (
     <main className={styles.loginPage}>
-      <section className={styles.loginSection}></section>
+      <section className={styles.loginSection}>
+        <div className={styles.loginContent}>
+          <div className={styles.loginInfos}>
+            <header className={styles.loginHeader}>
+              <p className={styles.loginTitle}>Bem-vindo a nossa comunidade!</p>
+              <p className={styles.loginSubTitle}>A maior comunidade de Yu-Gi-Oh do Brasil</p>
+            </header>
+            <figure className={styles.loginInfoFig}>
+              <img
+                src={require("../../Assets/all_millennium_items_render__legacy_of_the_duelist_by_maxiuchiha22_dcx3lvi-fullview.png")}
+                alt="all millennium items render legacy of the duelist fullview"
+                className={styles.milleniumImg}
+              />
+            </figure>
+          </div>
+          <div className={styles.loginBox}>
+            <p className={styles.login}>Login</p>
+            <span className={styles.loginInputs}>
+              <input type={"email"} placeholder="Digite seu email" className={styles.loginInput} />
+              <input type={"password"} placeholder="Digite sua senha" className={styles.loginInput} />
+              <span className={styles.passwordRecovery}>
+                <Link to={""}>Esqueci minha senha</Link>
+              </span>
+            </span>
+
+            <Button colorScheme={"blue"} size={"md"} width={"full"}>
+              Entrar
+            </Button>
+            <p>Ou registre-se com:</p>
+            <div className={styles.altLogin}>
+              <BsFacebook className={styles.BsFacebook} />
+              <SiGmail className={styles.SiGmail} />
+              <BsApple className={styles.BsApple} />
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
